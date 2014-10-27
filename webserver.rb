b966bddf1a551af10ea543d236788078146d3578
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-27 16:21:04 -0400

    working version

--- webserver.rb
+++ webserver.rb
@@ -1,57 +1,67 @@
 require 'socket'                                    # Require socket from Ruby Standard Library (stdlib)
-													# This allows access to TCP server
+                          # This allows access to TCP server
 
 host = 'localhost'
 port = 2000
 
 server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
 puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
-													# Sockets are bi-directional, motherfucker.
+                          # Sockets are bi-directional, motherfucker.
 
 loop do                                             # Server runs forever
   client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
-  													# CLient is a socket too in this instance.
+                            # CLient is a socket too in this instance.
 
   lines = []
   while (line = client.gets)  
   line = line.chomp
-  	break if line.empty? 							# Read the request and collect it until it's empty
+    break if line.empty?              # Read the request and collect it until it's empty
     lines << line
   end
   puts lines                                        # Output the full request to stdout
 
-  filename = lines[0].gsub(/GET \//, '').gsub(/ HTTP.*/, '')
-  
-  if File.exists?(filename)
-  	body = File.read(filename)
-  	headers << "HTTP/1.1 200 OK"
-
-  	
-  	
-
-  	headers << "Content-Type: text/html"
-	else
-  	body = "404: File not found\n"
- end
-
- headers = [								
-  "Content-Length: #{body.size}",
-  "Connection: Close"
-
- ]
-
-
-  body = Time.now.ctime
-
   
 
-headers.join("\r\n")
+
+
+
+
+filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+headers = []
+
+#response = ''
+if File.exists?(filename)
+  body = File.read(filename)
+  headers << "HTTP/1.1 200 OK"
+  if filename =~ /.css/
+    filetype = "text/css"
+  elsif filename =~ /.html/
+    filetype = "text/html"
+  else
+    filetype = "text/plain"
+  end
+
+  headers << "Content-Type: #{filetype}"
+else
+  body = "File not found\n"
+  
+  headers << "HTTP/1.1 404 Not Found"
+  headers << "Content-Type: text/plain"
+end
+
+headers << "Content-Lenght: #{body.size}"
+headers << "Connection: close"
+
+
+
+headers = headers.join("\r\n")
 response = [headers, body].join("\r\n\r\n")
 
-puts headers
-
-#response = headers + body
-
-  client.puts(response)                      		# Output the current time to the client
+  client.puts(response)                       # Output the current time to the client
   client.close                                      # Disconnect from the client
 end
+
+
+
+
+
